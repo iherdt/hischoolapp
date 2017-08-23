ec32643f94f04f66f0911c507489f57ee0f5aa50
Your Name <Samueltghenry@gmail.com>
2017-08-23 13:36:59 -0400

    Your message here

---
+++ src/pages/wizard/wizard.js
@@ -0,0 +1,10 @@
+const frm1 = document.getElementById('frm1');
+frm1.onclick = function() {
+	const demo = document.getElementById('demo');
+demo.style.display ='none';
+
+console.log('hello');
+};
+
+
+console.log('hello2');
