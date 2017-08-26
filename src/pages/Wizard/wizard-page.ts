328ac223d7cbd4727deb88d1b0e8f9df94164820
Eatashoe <gongwinston@gmail.com>
2017-08-25 20:25:37 -0400

    changes to the wizard

--- src/pages/Wizard/wizard-page.ts
+++ src/pages/Wizard/wizard-page.ts
@@ -1,9 +1,7 @@
 import { IonicPage } from 'ionic-angular';
 import { Component } from '@angular/core';
-import { NavController, MenuController } from 'ionic-angular';
-import { LocalStorage } from '../../providers/local-storage';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
-import { User } from '../../providers/user-service';
+
+import { FormGroup,} from '@angular/forms';
 
 
 @IonicPage()
